c15ec93df46e47912c4f641d78dbc0c9a9bf4ef1
SUJINJEONG012 <peekaboo32@naver.com>
2023-07-12 23:00:32 +0900

    app.컴포넌트에서 todo컴포넌트 props로 매개변수가 넘어가는 과정

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import Todo from './Todo';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [item, setItem] = useState({
+    id : "0",
+    title: "Hello React1 ",
+    done : true}
+    );
+
   return (
     <div className="App">
-      <Todo/>
+      <Todo item={item}/>
     </div>
   );
+
+  
 }
 
 export default App;
