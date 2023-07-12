5bf8301a5eed44ca5723760512fdfd3a7aa7a52d
SUJINJEONG012 <peekaboo32@naver.com>
2023-07-12 22:41:52 +0900

    Todo컴포넌트 아래에 export default Todo; 추가안해서 에러난거 수정

--- src/Todo.js
+++ src/Todo.js
@@ -1,10 +1,12 @@
 import React from "react";
 
-const Todo = ()=>{
+const Todo = () => {
     return (
         <div className="Todo">
             <input type="check" id="todo0" name="todo0" value="todo0" />
             <label for="todo0">Todo 컴포넌트 만들기</label>
         </div>
     )
-}
+};
+
+export default Todo;
