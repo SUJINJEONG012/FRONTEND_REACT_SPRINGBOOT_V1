065d8aad516f118ea05d496771cca157f82b4a35
SUJINJEONG012 <peekaboo32@naver.com>
2023-07-13 23:26:53 +0900

    onChange() 이벤트 핸들러로 onInputChange() 함수연결

--- src/AddTodo.js
+++ src/AddTodo.js
@@ -4,11 +4,22 @@ import { Button, Grid, TextField } from "@mui/material";
 const AddTodo = (props) => {
     //사용자의 입력을 저장할 오브젝트
     const[item, setItem] = useState({title: ""})
+   
+    // onInputChange 함수 작성, e는 매개변수, 사용자가 입력한 값
+    const onInputChange = (e)=>{
+        setItem({title : e.target.value});
+        console.log(item);
+    }
 
+
+
+    // onInputChange 함수 TextField에 연결 
     return (
         <Grid container style={{marginTop:20}}>
             <Grid xs={11} md={11} item style={{paddingRight:16}}>
-                <TextField placeholder="Add Todo here" fullWidth/>
+                 {/* onChange */}
+                <TextField placeholder="Add Todo here" fullWidth 
+                onChange={onInputChange} value ={item.title}/>
             </Grid>
             <Grid xs={1} md={1} item>
                 <Button fullWidth style={{height:'100%'}} color="secondary" variant="outlined">
