ece34719520889953c5a345f25b85c692de27977
angela <peekaboo32@naver.com>
2023-07-14 14:13:28 +0900

    onKeyPress ={enterKeyEventHandler}  키이벤트핸들러 함수 연결

--- src/AddTodo.js
+++ src/AddTodo.js
@@ -33,7 +33,9 @@ const AddTodo = (props) => {
             <Grid xs={11} md={11} item style={{paddingRight:16}}>
                  {/* onChange */}
                 <TextField placeholder="Add Todo here" fullWidth 
-                onChange={onInputChange} value ={item.title}/>
+                onChange={onInputChange}
+                onKeyPress ={enterKeyEventHandler}
+                value ={item.title}/>
             </Grid>
             <Grid xs={1} md={1} item>
                 <Button fullWidth style={{height:'100%'}} 
